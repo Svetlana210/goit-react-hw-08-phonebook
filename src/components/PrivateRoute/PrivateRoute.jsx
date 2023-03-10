eacc9c37591ff68ef70ba78d30b61b0d07b2b4bc
Svetlana210 <svetik.may24@gmail.com>
2023-03-10 15:10:04 +0100

    add token

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,9 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { isUserLogin } from '../../redux/auth/auth-selectors';
+import { getAuth } from '../../redux/auth/auth-selectors';
 const PrivateRoute = () => {
-  const isLogin = useSelector(isUserLogin);
-  if (!isLogin) {
+  const { isLogin, token } = useSelector(getAuth);
+  if (!isLogin && token) {
+    return <p>...Loading</p>;
+  }
+  if (!isLogin && !token) {
     return <Navigate to="/login" />;
   }
   return <Outlet />;
--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,8 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { isUserLogin } from '../../redux/auth/auth-selectors';
+import { getAuth } from '../../redux/auth/auth-selectors';
 const PublicRoute = () => {
-  const isLogin = useSelector(isUserLogin);
+  const { isLogin, token } = useSelector(getAuth);
+  if (!isLogin && token) {
+    return <p>...Loading</p>;
+  }
   if (isLogin) {
     return <Navigate to="/contacts" />;
   }
