1c23bf208c06e7279176e0be2d2acc43f53a3552
Svetlana210 <svetik.may24@gmail.com>
2023-03-10 15:15:37 +0100

    back

--- src/shared/services/auth.js
+++ src/shared/services/auth.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const authInstance = axios.create({
-  baseURL: 'https://connections-api.herokuapp.com',
-  // baseURL: 'https://auth-backend-lesson.herokuapp.com/api',
+  // baseURL: 'https://connections-api.herokuapp.com',
+  baseURL: 'https://auth-backend-lesson.herokuapp.com/api',
 });
 
 const setToken = token => {
